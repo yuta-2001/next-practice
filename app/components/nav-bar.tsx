11986ade0537e5d877397e183fd805ffed64f521
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-08-07 11:21:55 +0700

    navbar component

---
+++ app/components/nav-bar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function NavBar() {
+return (
+  <header　className="bg-gray-800 py-4">
+    <nav className="space-x-4">
+      <Link
+      href="/"
+      className="rounded bg-gray-700 px-3 py-2 text-white hober:bg-gray-500"
+      >
+      Home
+      </Link>
+      <Link
+      href="/blogs"
+      className="rounded bg-gray-700 px-3 py-2 text-white hober:bg-gray-500"
+      >
+      Nested Layout with Blogs
+      </Link>
+      <Link
+      href="/streaming-sr"
+      className="rounded bg-gray-700 px-3 py-2 text-white hober:bg-gray-500"
+      >
+      Streaming SR
+      </Link>
+      <Link
+      href="/auth"
+      className="rounded bg-gray-700 px-3 py-2 text-white hober:bg-gray-500"
+      >
+      Auth with crud
+      </Link>
+    </nav>
+  </header>
+)
+}
